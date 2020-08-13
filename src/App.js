c4d7fadef92d3a7cbd792ff1b6ffc776033006d3
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2020-08-13 00:08:59 -0400

    static version complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './components/Header'
+import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/WrongLetters.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WrongLetters = () => {
+  return (
+    <div>
+      <div class="wrong-letters-container">
+        <div id="wrong-letters"></div>
+      </div>      
+    </div>
+  )
+}
+
+export default WrongLetters
